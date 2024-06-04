e31ca5b33c59b845491bf95abae3cafd9f6c6387
Sebastian Speitel <sebastian.speitel@outlook.de>
2024-06-04 12:38:43 +0200

    feat(rr/typeset): export {STRING,BYTES}_LIKE constant sets

--- src/query/datafilter.rs
+++ src/query/datafilter.rs
@@ -167,7 +167,7 @@ impl<D: Data + ?Sized> Filter<D> for DataFilter {
                     }
                     #[inline]
                     fn accepting() -> impl crate::rr::typeset::TypeSet + 'static {
-                        crate::rr::typeset::StringLike::default()
+                        crate::rr::typeset::STRING_LIKE
                     }
                 }
 
